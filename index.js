0a707ec11c5f22e728774b9335dca1c7985fd815
zeddgrayhem <dev@zeddgrayhem.com>
2022-11-28 23:13:28 +0100

    added files

--- index.js
+++
@@ -1,6 +0,0 @@
-const StorageProvider = require('./storage.js')
-
-const store = StorageProvider({location: './storage', hi: true})
-const test_variable = new store.Storage('ok')
-
-test_variable.name = "Alexa"
